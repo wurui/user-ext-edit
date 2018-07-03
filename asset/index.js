a9697611ee71968199075a357680141a77fc77b4
wurui <wurui.wr@gmail.com>
2018-07-03 09:59:35 +0800

    fix

--- asset/index.js
+++ asset/index.js
@@ -1,72 +1,60 @@
 define(['require','oxjs'],function(require,OXJS){
   return {
     init:function($mod){
-    	var uploaderAvatar,uploaderImg;
-    	var avatarSrc,imgSrc;
+    	//var uploaderAvatar,uploaderImg;
+    	//var avatarSrc,imgSrc;
+    	var dataCache={},
+    	filechange=function(target){
+    		var 
+    		name=target.name,
+    		gen_id=target.id,
+    		datacache=dataCache[name];
+
+			if(datacache){
+				var uploader=datacache.uploader;
+				uploader.fileQ=[];
+				uploader.addToQ(target.files,function(arr){
+					var src=arr[0]._data;
+					//debugger
+					dataCache[name].src = src;
+					$('label[for='+gen_id+']',$mod).css('backgroundImage','url('+src+')');
+					//target.value='';
+                });
+
+			}else{
+				require(['oxm/wurui/image-uploader/0.3.0/asset/main'],function(uploader_main){
+    				dataCache[name] = {uploader:new uploader_main()};
+    				filechange(target);
+    			})
+			}
+    	};
+
     	$('form',$mod).on('change',function(e){
     		//console.log(e.target.name)
-    		var gen_id=e.target.id;
-    		switch(e.target.name){
-    			case 'file_avatar':
-    			
-    			if(uploaderAvatar){
-    				uploaderAvatar.fileQ=[];
-    				uploaderAvatar.addToQ(e.target.files,function(arr){
-    					var src=arr[0]._data;
-    					avatarSrc=src;
-    					$('label[for='+gen_id+']',$mod).css('backgroundImage','url('+src+')')
-                    });
-    			}else{
-    				require(['oxm/wurui/image-uploader/0.3.0/asset/main'],function(uploader_main){
-	    				//console.log(typeof uploader_main);
-	    				uploaderAvatar=new uploader_main();
-
-	    				uploaderAvatar.addToQ(e.target.files,function(arr){
-	    					var src=arr[0]._data;
-	    					avatarSrc=src;
-	    					
-	    					$('label[for='+gen_id+']',$mod).css('backgroundImage','url('+src+')')
-	                    });
-	    			})
-    			}
-    			break
-    			case 'file_img':
-    			if(uploaderImg){
-    				uploaderImg.addToQ(e.target.files,function(arr){
-    					var src=arr[0]._data;
-    					imgSrc=src;
-    					$('label[for='+gen_id+']',$mod).css('backgroundImage','url('+src+')')
-                    });
-    			}else{
-    				require(['oxm/wurui/image-uploader/0.3.0/asset/main'],function(uploader_main){
-	    				//console.log(typeof uploader_main);
-	    				uploaderImg=new uploader_main();
-	    				uploaderImg.addToQ(e.target.files,function(arr){
-	    					var src=arr[0]._data;
-	    					//debugger
-	    					imgSrc=src;
-	    					$('label[for='+gen_id+']',$mod).css('backgroundImage','url('+src+')')
-	                    });
-	    			})
-    			}
-    			
-    			break
+    		var tar=e.target;
+    		if(tar.type=='file'){
+    			filechange(tar);
     		}
     	}).on('submit',function(e){
     		var f=e.target;
     		try{
+
     			var data={
     				'user-ext':{
 	    				uid:OXJS.getUID(),
-	    				avatar:avatarSrc,
-	    				img:imgSrc,
+	    				avatar:dataCache.file_avatar && dataCache.file_avatar.src,
+	    				img:dataCache.file_img && dataCache.file_img.src,
 	    				slogan:f.slogan.value,
 	    				sex:f.sex.value,
 	    				nick:f.nick.value
 	    			}
     			}
-
-    			$mod.OXPost(data)
+    			if(f.uid.value){
+    				$mod.OXPut(data)
+    			}else{
+    				$mod.OXPost(data)
+    			}
+    			
     		}catch(er){
     			console.log(er)
     		}
